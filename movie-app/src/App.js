16cdaff16d2d3422dd9a330544ca3ce16afcf885
Rajendra <gomeshwarr@gmail.com>
2022-12-09 00:25:07 +0530

    search movie

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,15 +1,74 @@
+import React, {useState} from 'react';
+import styled from "styled-components";
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Axios from 'axios';
 import Header from './component/header/Header';
 import Home from './pages/home/home';
 import MovieList from './component/movieList/movieList';
 import Movie from './pages/movieDetail/movie';
+import MovieComponent from './component/movieComponent/movieComponent';
+export const API_KEY = "a9118a3a";
+
+
+
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;;
+`;
+
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
 
 function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+  const [isSearch, setIsSearch]= useState(false)
+
+  const [movieList, updateMovieList] = useState([]);
+  const [selectedMovie, onMovieSelect] = useState();
+
+  const [timeoutId, updateTimeoutId] = useState();
+  const fetchData = async (searchString) => {
+    const response = await Axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`,
+    );
+    updateMovieList(response.data.Search);
+  };
+
+  const onTextChange = (e) => {
+    onMovieSelect("")
+    clearTimeout(timeoutId);
+    updateSearchQuery(e.target.value);
+    const timeout = setTimeout(() => fetchData(e.target.value), 500);
+    updateTimeoutId(timeout);
+    setIsSearch(!isSearch);
+  };
   return (
     <div className="App">
         <Router>
-          <Header />
+          <Header searchQuery={searchQuery}
+            onTextChange={onTextChange}/>
+           {isSearch ? <MovieListContainer>
+        {movieList?.length ? (
+          movieList.map((movie, index) => (
+            <MovieComponent
+              key={index}
+              movie={movie}
+              onMovieSelect={onMovieSelect}
+            />
+          ))
+        ) : (
+          <Placeholder src="/react-movie-app/movie-icon.svg" />
+        )}
+      </MovieListContainer>: ""}
             <Routes>
                 <Route index element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
--- movie-app/src/component/header/Header.js
+++ movie-app/src/component/header/Header.js
@@ -3,14 +3,17 @@ import "./Header.css"
 import { Link } from "react-router-dom"
 import movieIcon from "../../images/pngaaa.com-2989054.png"
 
-const Header = () => {
+const Header = (props) => {
     return (
         <div className="header">
             <div className="headerLeft">
                 <Link to="/"><img className="header__icon" src={movieIcon} /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+                <Link to="/movies/popular" style={{ textDecoration: "none" }}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{ textDecoration: "none" }}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{ textDecoration: "none" }}><span>Upcoming</span></Link>
+                <form class="nosubmit">
+                    <input class="nosubmit" type="search" placeholder="Search..." value={props.searchQuery} onChange={props.onTextChange}/>
+                </form>
             </div>
         </div>
     )
