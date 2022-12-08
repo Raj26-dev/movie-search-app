247764a1dd58305fb4b3734344f0c061657ae271
Rajendra <gomeshwarr@gmail.com>
2022-12-08 17:42:23 +0530

    movieList HomePage movieDetailPage

---
+++ movie-app/src/component/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
+import movieIcon from "../../images/pngaaa.com-2989054.png"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header__icon" src={movieIcon} /></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
